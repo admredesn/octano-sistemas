819518bfc9bf186b68d0287ae21a63b131328d4d
admredesn <admredesn@gmail.com>
2026-05-31 15:50:51 -0300

    add js/auth.js

---
+++ js/auth.js
@@ -0,0 +1,19 @@
+
+const { createClient } = supabase;
+const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function login(email, senha) {
+  const { data, error } = await sb.auth.signInWithPassword({ email, senha });
+  if (error) throw error;
+  return data;
+}
+
+async function logout() {
+  await sb.auth.signOut();
+  location.reload();
+}
+
+async function getSession() {
+  const { data } = await sb.auth.getSession();
+  return data.session;
+}
